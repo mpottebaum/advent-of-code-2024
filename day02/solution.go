4fd10e5d20528ec5068ebaa1a73141313691974e
Mike Pottebaum <michael.pottebaum@cdw.com>
2024-12-02 23:10:22 -0600

    day02 pt 2

--- day02/solution.go
+++ day02/solution.go
@@ -7,6 +7,34 @@ import (
 	"strings"
 )
 
+func isSafe(nums []int) bool {
+	if len(nums) < 2 {
+		return false
+	}
+	var isIncreasing bool
+	for x := 0; x < len(nums); x++ {
+		if x != len(nums)-1 {
+			numA := nums[x]
+			numB := nums[x+1]
+			diff := numB - numA
+			if diff == 0 {
+				return false
+			}
+			if math.Abs(float64(diff)) > 3 {
+				return false
+			}
+			if x == 0 {
+				isIncreasing = diff > 0
+			} else if isIncreasing && diff < 0 {
+				return false
+			} else if !isIncreasing && diff > 0 {
+				return false
+			}
+		}
+	}
+	return true
+}
+
 func Solve(inputFile string) {
 	fileName := "input"
 	if len(inputFile) > 0 {
@@ -27,30 +55,11 @@ func Solve(inputFile string) {
 			}
 			nums = append(nums, num)
 		}
-		var isIncreasing bool
 		for x := 0; x < len(nums); x++ {
-			if len(nums) < 2 {
-				break
-			}
-			if x == len(nums)-1 {
+			newSlice := utils.RemoveIndex(nums, x)
+			if isSafe(newSlice) {
 				numSafe += 1
-			} else {
-				numA := nums[x]
-				numB := nums[x+1]
-				diff := numB - numA
-				if diff == 0 {
-					break
-				}
-				if math.Abs(float64(diff)) > 3 {
-					break
-				}
-				if x == 0 {
-					isIncreasing = diff > 0
-				} else if isIncreasing && diff < 0 {
-					break
-				} else if !isIncreasing && diff > 0 {
-					break
-				}
+				break
 			}
 		}
 	}
