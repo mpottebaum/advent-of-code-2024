45605d09b0a9850d76bc23e9389314c3d508a29d
Mike Pottebaum <michael.pottebaum@cdw.com>
2024-12-06 12:34:27 -0600

    day06 pt 2

--- day06/solution.go
+++ day06/solution.go
@@ -7,33 +7,16 @@ import (
 	"strings"
 )
 
-func stringifyPos(row int, col int) string {
+func stringifyOppDir(row int, col int, dir string) string {
 	rowStr := strconv.Itoa(row)
 	colStr := strconv.Itoa(col)
-	return "r" + rowStr + "c" + colStr
+	return "r" + rowStr + "c" + colStr + dir
 }
 
-func Solve(inputFile string) {
-	input := utils.ReadFileToString("day06/" + inputFile + ".txt")
-	rows := strings.Split(input, "\n")
-	rows = rows[:len(rows)-1]
-	var guardRow, guardCol int
-getGuard:
-	for i := 0; i < len(rows); i++ {
-		row := rows[i]
-		for x := 0; x < len(row); x++ {
-			col := row[x]
-			if string(col) == "^" {
-				guardRow = i
-				guardCol = x
-				break getGuard
-			}
-		}
-	}
-	startStr := stringifyPos(guardRow, guardCol)
-	posSet := map[string]bool{
-		startStr: true,
-	}
+func isLoopy(startRow int, startCol int, oppRow int, oppCol int, rows []string) bool {
+	oppDirSet := map[string]bool{}
+	guardRow := startRow
+	guardCol := startCol
 	isWalking := true
 	direction := "^"
 	for isWalking {
@@ -41,13 +24,17 @@ getGuard:
 		case "^":
 			for guardRow -= 1; guardRow >= 0; guardRow-- {
 				char := rows[guardRow][guardCol]
-				if string(char) == "#" {
+				if string(char) == "#" || (guardRow == oppRow && guardCol == oppCol) {
+					stringerBell := stringifyOppDir(guardRow, guardCol, direction)
+					if exists := oppDirSet[stringerBell]; exists {
+						return true
+					} else {
+						oppDirSet[stringerBell] = true
+					}
 					direction = ">"
 					guardRow += 1
 					break
 				} else {
-					stringerBell := stringifyPos(guardRow, guardCol)
-					posSet[stringerBell] = true
 					if guardRow == 0 {
 						isWalking = false
 					}
@@ -56,13 +43,17 @@ getGuard:
 		case "v":
 			for guardRow += 1; guardRow < len(rows); guardRow++ {
 				char := rows[guardRow][guardCol]
-				if string(char) == "#" {
+				if string(char) == "#" || (guardRow == oppRow && guardCol == oppCol) {
+					stringerBell := stringifyOppDir(guardRow, guardCol, direction)
+					if exists := oppDirSet[stringerBell]; exists {
+						return true
+					} else {
+						oppDirSet[stringerBell] = true
+					}
 					direction = "<"
 					guardRow -= 1
 					break
 				} else {
-					stringerBell := stringifyPos(guardRow, guardCol)
-					posSet[stringerBell] = true
 					if guardRow == len(rows)-1 {
 						isWalking = false
 					}
@@ -72,13 +63,17 @@ getGuard:
 			row := rows[guardRow]
 			for guardCol += 1; guardCol < len(row); guardCol++ {
 				char := rows[guardRow][guardCol]
-				if string(char) == "#" {
+				if string(char) == "#" || (guardRow == oppRow && guardCol == oppCol) {
+					stringerBell := stringifyOppDir(guardRow, guardCol, direction)
+					if exists := oppDirSet[stringerBell]; exists {
+						return true
+					} else {
+						oppDirSet[stringerBell] = true
+					}
 					direction = "v"
 					guardCol -= 1
 					break
 				} else {
-					stringerBell := stringifyPos(guardRow, guardCol)
-					posSet[stringerBell] = true
 					if guardCol == len(row)-1 {
 						isWalking = false
 					}
@@ -87,13 +82,17 @@ getGuard:
 		case "<":
 			for guardCol -= 1; guardCol >= 0; guardCol-- {
 				char := rows[guardRow][guardCol]
-				if string(char) == "#" {
+				if string(char) == "#" || (guardRow == oppRow && guardCol == oppCol) {
+					stringerBell := stringifyOppDir(guardRow, guardCol, direction)
+					if exists := oppDirSet[stringerBell]; exists {
+						return true
+					} else {
+						oppDirSet[stringerBell] = true
+					}
 					direction = "^"
 					guardCol += 1
 					break
 				} else {
-					stringerBell := stringifyPos(guardRow, guardCol)
-					posSet[stringerBell] = true
 					if guardCol == 0 {
 						isWalking = false
 					}
@@ -101,6 +100,38 @@ getGuard:
 			}
 		}
 	}
-	numbPosers := len(posSet)
-	fmt.Println("numb posers", numbPosers)
+	return false
+}
+
+func Solve(inputFile string) {
+	input := utils.ReadFileToString("day06/" + inputFile + ".txt")
+	rows := strings.Split(input, "\n")
+	rows = rows[:len(rows)-1]
+	var startRow, startCol int
+getGuard:
+	for i := 0; i < len(rows); i++ {
+		row := rows[i]
+		for x := 0; x < len(row); x++ {
+			col := row[x]
+			if string(col) == "^" {
+				startRow = i
+				startCol = x
+				break getGuard
+			}
+		}
+	}
+	numbLoopies := 0
+	for i := 0; i < len(rows); i++ {
+		row := rows[i]
+		for x := 0; x < len(row); x++ {
+			char := row[x]
+			if string(char) != "#" {
+				if loopy := isLoopy(startRow, startCol, i, x, rows); loopy {
+					numbLoopies += 1
+				}
+			}
+		}
+	}
+
+	fmt.Println("numb loopies", numbLoopies)
 }
